b65c544939806d6bbd1212cf0498f9e851a8f515
Florian ALONSO <florian@neopixl.com>
2019-05-07 11:10:14 +0200

    init

---
+++ app/src/main/java/fr/hetic/h2p2022/Example.kt
@@ -0,0 +1,14 @@
+package fr.hetic.h2p2022
+
+import android.util.Log
+
+/**
+ * Created by Florian ALONSO (Neopixl SA).
+ * Copyright © 2019 H2P2022. All rights reserved.
+ */
+
+
+
+fun test() {
+    
+}
