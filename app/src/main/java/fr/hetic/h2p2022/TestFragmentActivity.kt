efd66affd7b12ee2527e4f193cb3cc5c85f4b7ca
Florian ALONSO <florian@neopixl.com>
2019-05-28 14:20:53 +0200

    Fragment end

--- app/src/main/java/fr/hetic/h2p2022/TestFragmentActivity.kt
+++ app/src/main/java/fr/hetic/h2p2022/TestFragmentActivity.kt
@@ -2,7 +2,9 @@ package fr.hetic.h2p2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import fr.hetic.h2p2022.fragment.TestFragment
+import kotlinx.android.synthetic.main.activity_test_fragment.*
 
 class TestFragmentActivity : AppCompatActivity() {
 
@@ -15,10 +17,18 @@ class TestFragmentActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction()
             .add(R.id.fragmentContainer, module)
+            .addToBackStack("one")
             .commit()
 
         supportFragmentManager.beginTransaction()
             .add(R.id.bottomFragmentContainer, moduleTwo)
+            .addToBackStack("two")
             .commit()
+
+        fragmentContainer.postDelayed({
+
+            supportFragmentManager.popBackStack("one", FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+        }, 10000)
     }
 }
